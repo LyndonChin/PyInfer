8579bfa282a024b79c9c40df139a07e02cadcb52
liangfei <njliangfei@gmail.com>
2015-07-09 11:09:22 +0800

    create pyinfer

--- generator/generator-comprehension.py
+++
@@ -1,7 +0,0 @@
-# list comprehension
-doubles = [2 * n for n in range(50)]
-print(doubles)
-
-# same as the list comprehension above
-doubles = list(2 * n for n in range(50))
-print(doubles)
--- generator/range_xrange.py
+++
@@ -1,9 +0,0 @@
-# Note: Python 2.x only
-# using a non-generator
-
-sum_of_first_n = sum(range(1000000))
-print(sum_of_first_n)
-
-# using a generator
-sum_of_first_n = sum(xrange(1000000))
-print(sum_of_first_n)
